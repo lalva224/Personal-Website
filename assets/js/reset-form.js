aee4cf0da8a4daa6a6e42da1bc68fd170b1f0923
lalva224 <alvarezleandro720@gmail.com>
2024-07-26 13:30:18 -0400

    added favicon and contact form

---
+++ assets/js/reset-form.js
@@ -0,0 +1,8 @@
+// let goBack = document.getElementById('back')
+// let form = document.getElementById('myform')
+// goBack.addEventListener('click',function(){
+//     form.reset()
+// })
+// window.addEventListener('beforeunload',function(){
+//     form.reset()
+// })
